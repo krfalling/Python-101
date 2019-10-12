94ef8ee5ef589ec2efcbe32e93c57580372aaf04
krfalling <49820761+krfalling@users.noreply.github.com>
2019-10-12 15:22:42 -0400

    Add files via upload

---
+++ MIT Phyton 101 Prob Set 1-2.py
@@ -0,0 +1,19 @@
+s = 'azcbobobobegghakl'
+num = 0
+
+for i in range(len(s)):
+    
+    if s [i:i + 3] == 'bob':
+        
+        num += 1
+    
+print ('Number of times bob occurs is: ' + str (num))
+    
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 14:14:24 2019
+
+@author: krfalling
+"""
+
