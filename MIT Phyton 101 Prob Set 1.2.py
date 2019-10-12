f53b8ff4f708da2a8efcfa25380f895d75c15a14
krfalling <49820761+krfalling@users.noreply.github.com>
2019-10-12 15:20:00 -0400

    Delete MIT Phyton 101 Prob Set 1.2.py

--- MIT Phyton 101 Prob Set 1.2.py
+++
@@ -1,9 +0,0 @@
-s = 'azcbobobobobegghakl'
-print ('Number of times bob occurs is: ' + str (s. index ('bob')))#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct 12 14:46:54 2019
-
-@author: krfalling
-"""
-
